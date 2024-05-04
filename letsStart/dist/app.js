35f17c581b03fc3e4e93b95a6818344a444d21d2
Byeong Min Cho <merrybmc@gmail.com>
2024-05-05 04:26:51 +0900

    read, create

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
+app.use(express.json());
 var data = [1, 2, 3, 4];
 app.listen(port, function () {
     console.log('server connect');
@@ -17,5 +18,42 @@ app.use(function (req, res, next) {
 });
 app.get('/cats/blue', function (req, res) {
     res.send({ blue: app_model_1.Cat[0] });
+});
+app.get('/cats', function (req, res) {
+    try {
+        var cats = app_model_1.Cat;
+        if (!cats)
+            throw new Error('empty in cats');
+        res.status(200).json({ data: cats });
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+app.get('/cats/:id', function (req, res) {
+    try {
+        var id_1 = req.params.id;
+        if (!id_1)
+            throw new Error('cannot find id');
+        var cat = app_model_1.Cat.find(function (cat) { return cat.id === id_1; });
+        if (!id_1)
+            throw new Error('cannot find data');
+        res.status(200).json({ cat: cat });
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+app.post('/cats', function (req, res) {
+    try {
+        var data_1 = req.body;
+        if (!data_1)
+            throw new Error('cannot read data');
+        app_model_1.Cat.push(data_1);
+        res.status(200).json({ Cat: app_model_1.Cat });
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 //# sourceMappingURL=app.js.map
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
 import { Cat, CatType } from './app.model';
+import { error } from 'console';
 
 const app = express();
 const port = 8000;
+
+app.use(express.json());
 
 const data = [1, 2, 3, 4];
 
@@ -14,7 +17,7 @@ app.get('/', (req, res) => {
   res.send({ Cat });
 });
 
-// middleware
+// logging middleware
 // 코드 순서에 영향을 받음
 app.use((req, res, next: express.NextFunction) => {
   console.log('this is middleware');
@@ -23,4 +26,51 @@ app.use((req, res, next: express.NextFunction) => {
 
 app.get('/cats/blue', (req, res) => {
   res.send({ blue: Cat[0] });
+});
+
+// CRUD
+
+// READ 모든 데이터 조회
+app.get('/cats', (req, res) => {
+  try {
+    const cats = Cat;
+    if (!cats) throw new Error('empty in cats');
+
+    res.status(200).json({ data: cats });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// READ 특정 데이터 조회
+// dynamic routing
+app.get('/cats/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) throw new Error('cannot find id');
+
+    const cat = Cat.find((cat) => cat.id === id);
+
+    if (!id) throw new Error('cannot find data');
+
+    res.status(200).json({ cat });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// CREATE
+app.post('/cats', (req, res) => {
+  try {
+    const data = req.body;
+
+    if (!data) throw new Error('cannot read data');
+
+    Cat.push(data);
+
+    res.status(200).json({ Cat });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
 });

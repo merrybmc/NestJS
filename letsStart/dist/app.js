1461d1e481b113b73e75fb542d7931f035ff3d64
Byeong Min Cho <merrybmc@gmail.com>
2024-05-05 04:51:18 +0900

    singleton 디자인 패턴

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -2,18 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cats_route_1 = require("./cats/cats.route");
-var app = express();
-var port = 8000;
-app.use(express.json());
-app.listen(port, function () {
-    console.log('server connect');
-});
-app.get('/', function (req, res) {
+var Server = (function () {
+    function Server() {
+        var app = express();
+        this.app = app;
+    }
+    Server.prototype.setRoute = function () {
+        this.app.use('/cats', cats_route_1.default);
+    };
+    Server.prototype.setMiddleWare = function () {
+        this.app.use(express.json());
+        this.setRoute();
+        this.app.use(function (req, res, next) {
+            console.log('this is middleware');
+            next();
+        });
+    };
+    Server.prototype.listen = function () {
+        var port = 8000;
+        this.app.listen(port, function () {
+            console.log('server connect');
+        });
+    };
+    return Server;
+}());
+var server = new Server();
+server.listen();
+server.app.get('/', function (req, res) {
     res.send('hello world');
 });
-app.use(function (req, res, next) {
-    console.log('this is middleware');
-    next();
-});
-app.use('/cats', cats_route_1.default);
 //# sourceMappingURL=app.js.map
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,25 +1,51 @@
 import * as express from 'express';
 import catsRouter from './cats/cats.route';
 
-const app = express();
-const port = 8000;
+// singleton pattern
+// 객체의 인스턴스가 오직 1개만 생성되게 하는 패턴
+// 클래스로 인스턴스를 1개만 생성하는 패턴
+// 최초 한 번의 new 연산자를 통해 객체를 만들고 추후 객체에 접근을 할 때 메모리 낭비를 방지
+// 다른 클래스간의 데이터 공유가 쉬움
 
-app.use(express.json());
+class Server {
+  public app: express.Application;
 
-app.listen(port, () => {
-  console.log('server connect');
-});
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-app.get('/', (req, res) => {
+  private setRoute() {
+    // route 분리
+    this.app.use('/cats', catsRouter);
+  }
+
+  private setMiddleWare() {
+    // json middleWare
+    this.app.use(express.json());
+
+    this.setRoute();
+
+    // logging middleware
+    // 코드 순서에 영향을 받음
+    this.app.use((req, res, next: express.NextFunction) => {
+      console.log('this is middleware');
+      next();
+    });
+  }
+
+  public listen() {
+    const port = 8000;
+
+    this.app.listen(port, () => {
+      console.log('server connect');
+    });
+  }
+}
+
+const server = new Server();
+server.listen();
+
+server.app.get('/', (req, res) => {
   res.send('hello world');
 });
-
-// logging middleware
-// 코드 순서에 영향을 받음
-app.use((req, res, next: express.NextFunction) => {
-  console.log('this is middleware');
-  next();
-});
-
-// route 분리
-app.use('/cats', catsRouter);

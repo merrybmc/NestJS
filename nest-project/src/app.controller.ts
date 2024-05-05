a4288de2f12ae222a9b40739a2396f6b47da8324
Byeong Min Cho <merrybmc@gmail.com>
2024-05-06 03:51:47 +0900

    controller get basic

--- nest-project/src/app.controller.ts
+++ nest-project/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
-@Controller()
+// @Controller() 데코레이터
+// express의 router 기능
+// @Controller() === "/"
+// @Controller("cats") === "/cats"
+@Controller('cats')
 export class AppController {
+  // 서비스 의존성 주입
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()데코레이터
+  // express의 app.get()
+  // @Get() === route.get("/")
+  // @Get("hello") === route.get("/hello")
+  @Get('hello/:id')
+  getHello(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() Body,
+    @Param() Param: { id: string }, // type 지정
+    @Query() Query,
+  ): string {
+    // const body = req.body; // next.js에서도 가능 (express)
+    const body = Body; // nest 축약 버전 - const body =req.body; 와 같음
+
+    // const param = req.params; // next.js에서도 가능 (express)
+    const param = Param; // nest 축약 버전 - const param = req.params; 와 같음
+
+    // const query = req.query; // next.js에서도 가능 (express)
+    const query = Query; // nest 축약 버전 - const query - req.query; 와 같음
+
+    // 서비스 코드에서 사용할 데이터를 인자로 넘겨주기
+    return this.appService.getHello(body, param);
   }
 }
